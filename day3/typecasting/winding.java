005c5a5fecd4a971723a6b0b6ad7fd9d417a8fea
SahilGawade <sahilgawade828@gmail.com>
2025-03-21 09:48:24 +0530

    Create winding.java

---
+++ day3/typecasting/winding.java
@@ -0,0 +1,11 @@
+public class winding
+  {
+    public static void main(string args[])
+    {
+      int a=9;
+      double b=a;
+      system.out.println(a);
+      system.out.println(b);
+      
+    }
+  }
