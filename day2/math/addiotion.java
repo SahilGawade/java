bc1e6939da28c640ce8d49404b7a9bf0d5539f61
SahilGawade <sahilgawade828@gmail.com>
2025-03-21 09:31:35 +0530

    Create addiotion.java

---
+++ day2/math/addiotion.java
@@ -0,0 +1,13 @@
+public class Num {
+    public static void main(String[] args) {
+        String num1=args[0];
+        String num2=args[1];
+        
+        int a=Integer.parseInt(num1);
+        int b=Integer.parseInt(num2);
+        
+        System.out.println("The addition="+(a+b));
+        System.out.println(a + " + " + b + " = " + (a + b)); 
+    }
+    
+}
